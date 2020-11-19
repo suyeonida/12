0f64311415b9b6774bd12e0078e7550b29cd3b26
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-11-19 16:36:23 +0900

    7

--- main2.c
+++ main2.c
@@ -6,19 +6,32 @@
 
 int main(int argc, char *argv[]) {
 	FILE *fp;
-	char c;
-	//char str[100]; 뭉탱이로 가지고 올 떄  
+	char str[100];
+	char path[100];
+	char lookup[100];
+	
+	printf("input file path:");
+	scanf("%s",path);
+	printf("input search word:");
+	scanf("%s",lookup);
 	int i;
 	
 	fp= fopen("sample.txt","r");
 	if (fp==NULL){
+		printf("invalid path! (%s)\n",path);
+	
 		return -1;
 	}
 	
-	while( (c=fgetc(fp)) != EOF ){  //한글자씩 읽어 저장  
-		printf("%c",c);
+	while( fgets(str,100,fp) !=NULL){ 
+		if(strncmp(str,lookup,strlen(lookup)) ==0){
+			printf("search succeed!\n");
+			fclose(fp);
+			return 0;
+		}
+		
 	}
-	
+	printf("search failed!\n");
 	fclose(fp);
 	
 	return 0;
