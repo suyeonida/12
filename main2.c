08b4ddcd614dc7bbf8b4b2a5c89c8e7752326749
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-11-19 16:16:17 +0900

    6

--- main2.c
+++ main2.c
@@ -6,19 +6,18 @@
 
 int main(int argc, char *argv[]) {
 	FILE *fp;
-	char str[100];
+	char c;
+	//char str[100]; 뭉탱이로 가지고 올 떄  
 	int i;
 	
-	fp= fopen("sample.txt","w");
+	fp= fopen("sample.txt","r");
 	if (fp==NULL){
 		return -1;
 	}
 	
-	for(i=0;i<3;i++){
-		
-	printf("Input a word:");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);}
+	while( (c=fgetc(fp)) != EOF ){  //한글자씩 읽어 저장  
+		printf("%c",c);
+	}
 	
 	fclose(fp);
 	
